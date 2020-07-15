899ddf87c168af1d78bf0f82e30d644d950fa005
hawk588 <gdhawk588@gmail.com>
2020-07-15 12:57:07 -0500

    questions page

--- QuizApp/Controller/QuestionPageViewController.swift
+++ QuizApp/Controller/QuestionPageViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class QuestionPageViewController: UIViewController {
 
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
